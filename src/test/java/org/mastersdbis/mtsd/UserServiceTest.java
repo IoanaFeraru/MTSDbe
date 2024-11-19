5efde4b284b8c9c2fd17079ffc2d599de81d50dc
Steftrix <zaborilastefan5@gmail.com>
2024-11-19 23:20:53 +0200

    Update + Delete for UserService

--- src/test/java/org/mastersdbis/mtsd/UserServiceTest.java
+++ src/test/java/org/mastersdbis/mtsd/UserServiceTest.java
@@ -21,7 +21,7 @@ class UserServiceTest {
     void addUser() {
         // Creează utilizatorul
         User newUser = new User();
-        newUser.setUsername("Stefan");
+        newUser.setUsername("Ioana");
         newUser.setPassword("Parola11!");
         newUser.setEmail("stefan@gmail.com");
         newUser.setPhoneNumber("+40 789678567");
@@ -30,13 +30,37 @@ class UserServiceTest {
         // Salvează utilizatorul
         userService.addUser(newUser);
 
-        User savedUser = userService.findByUsername("Stefan");
-        System.out.println("Detalii utilizator salvat: " + savedUser.toString());
+        User savedUser = userService.findByUsername("Ioana");
+        System.out.println("Detalii utilizator salvat: \n" + savedUser.toString());
 
         String encodedPassword = savedUser.getPassword();
 
         boolean passwordMatches = passwordEncoder.matches("Parola11!", encodedPassword);
 
         Assertions.assertTrue(passwordMatches, "Parola nu a fost codificată corect.");
+    }
+    @Test
+    void updateUser() {
+        User userFromDb = userService.findByUsername("Stefan");
+        Assertions.assertNotNull(userFromDb, "Utilizatorul cu username-ul 'Ioana' nu a fost găsit în baza de date.");
+        Assertions.assertEquals("Stefan", userFromDb.getUsername(), "Username-ul curent nu corespunde celui așteptat.");
+        String newUsername = "Ioana";
+        userFromDb.setUsername(newUsername);
+        userService.updateUser(userFromDb);
+        User updatedUser = userService.findByUsername(newUsername);
+        Assertions.assertNotNull(updatedUser, "Utilizatorul cu noul username 'IoanaUpdated' nu a fost găsit în baza de date.");
+        Assertions.assertEquals(newUsername, updatedUser.getUsername(), "Username-ul nu a fost actualizat corect.");
+        User oldUser = userService.findByUsername("Stefan");
+        Assertions.assertNull(oldUser, "Utilizatorul cu vechiul username 'Ioana' există încă în baza de date.");
+        System.out.println("Username-ul a fost actualizat cu succes.");
+    }
+    @Test
+    void deleteUser() {
+        User userFromDb = userService.findByUsername("Ioana");
+        Assertions.assertNotNull(userFromDb, "Utilizatorul nu a fost găsit în baza de date.");
+        userService.deleteUser(userFromDb);
+        User deletedUser = userService.findByUsername("Ioana");
+        Assertions.assertNull(deletedUser, "Utilizatorul nu a fost sters din baza de date.");
+        System.out.println("Utilizatorul a fost sters cu succes.");
     }
 }
