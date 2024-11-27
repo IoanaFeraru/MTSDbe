7b4629357c0ea72117e2c03a95f1a3067e957864
Steftrix <zaborilastefan5@gmail.com>
2024-11-27 23:42:46 +0200

    BookingTest

--- src/test/java/org/mastersdbis/mtsd/Services/BookingServiceTest.java
+++ src/test/java/org/mastersdbis/mtsd/Services/BookingServiceTest.java
@@ -16,6 +16,9 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 @SpringBootTest
 class BookingServiceTest {
 
@@ -42,8 +45,21 @@ class BookingServiceTest {
 
         // Assert: Verify the booking was saved correctly
         Assertions.assertNotNull(booking, "Booking-ul nu a fost creat.");
-        Assertions.assertEquals(dueDate, booking.getDueDate(), "Data booking-ului nu este corectă.");
+        assertEquals(dueDate, booking.getDueDate(), "Data booking-ului nu este corectă.");
 
         System.out.println("Booking adăugat cu succes: " + booking);
+    }
+    @Test
+    void updateBooking() {
+        Booking existingBooking = bookingService.findAllBookings().getLast();
+        existingBooking.setDeliveryAddress("Str. Dealul Zorilor");
+        bookingService.updateBooking(existingBooking);
+        assertEquals("Str. Dealul Zorilor", existingBooking.getDeliveryAddress(),"Adresa nu a fost actualizata");
+        System.out.println("Adresa de booking a fost actualizata cu succes!");
+    }
+    @Test
+    void cancelBooking() {
+        Booking existingBooking = bookingService.findAllBookings().getFirst();
+
     }
 }
